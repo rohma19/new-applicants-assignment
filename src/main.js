988292f0a201c85a4ad8aef3f07b24d74a3585a3
Rohma Enggardini <rohmaenggardini@gmail.com>
2023-11-16 12:04:42 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-// Add your JavaScript here
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+let currentCalculation = '';
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.innerText;
+
+        if (buttonValue === '=') {
+            try {
+                const result = eval(currentCalculation);
+                display.value = result;
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else if (buttonValue === 'AC') {
+            display.value = '';
+            currentCalculation = '';
+        } else if (buttonValue === 'DEL') {
+            currentCalculation = currentCalculation.slice(0, -1);
+            display.value = currentCalculation;
+        } else {
+            currentCalculation += buttonValue;
+            display.value = currentCalculation;
+        }
+    });
+});
